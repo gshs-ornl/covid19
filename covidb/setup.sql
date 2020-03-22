8fa0ab9a826c9c03d9784013a90c03484ae8eafa
6nf <grantjn@ornl.gov>
2020-03-22 04:16:45 -0400

    finish flushing out schema for covidb database

--- covidb/setup.sql
+++ covidb/setup.sql
@@ -15,14 +15,49 @@ CREATE DATABASE covidb WITH OWNER cvadmin;
 GRANT CONNECT ON DATABASE covidb TO ingester, digester, librarian, historian
                                     guest;
 CREATE SCHEMA IF NOT EXISTS scraping AUTHORIZATION jesters
-  CREATE TABLE IF NOT EXISTS raw 
+  CREATE TABLE IF NOT EXISTS raw_data
   (country varchar, state varchar, url varchar, raw_page text, 
-   access_time timestamp, county varchar, cases integer,
-   udpated timestamp with time zone, deaths integer, presumptive integer, 
-   recovered integer, tested integer, hospitalized integer, negative integer,
-   counties integer, severe integer, lat numeric, lon numeric, 
-   parish varchar, monitored integer, no_longer_monitored integer, 
-   pending_tests integer, active integer);
+   access_time timestamp, county varchar DEFAULT NULL, 
+   cases integer DEFAULT NULL, udpated timestamp with time zone, 
+   deaths integer DEFAULT NULL, presumptive integer DEFAULT NULL, 
+   recovered integer DEFAULT NULL, tested integer DEFAULT NULL, 
+   hospitalized integer DEFAULT NULL, negative integer DEFAULT NULL,
+   counties integer DEFAULT NULL, severe integer DEFAULT NULL, 
+   lat numeric DEFAULT NULL, lon numeric DEFAULT NULL, 
+   parish varchar DEFAULT NULL, monitored integer DEFAULT NULL, 
+   no_longer_monitored integer DEFAULT NULL,  
+   pending_tests integer DEFAULT NULL, active integer DEFAULT NULL,
+   inconclusive integer DEFAULT NULL, scrape_group integer NOT NULL)
+ CREATE TABLE IF NOT EXISTS pages
+  (id SERIAL PRIMARY KEY, page text, url varchar, 
+   access_time timestamp with time zone)
+ CREATE TABLE IF NOT EXISTS scrape_group
+  (id SERIAL PRIMARY KEY, scrape_group integer NOT NULL)
+ CREATE TABLE IF NOT EXISTS pages
+  (id SERIAL PRIMARY KEY, url varchar NOT NULL, page text NOT NULL,
+   updated timestamp with timezone)
+ CREATE TABLE IF NOT EXISTS state_data
+  (country_id REFERENCES static.country(id),
+   state_id REFERENCES static.state(id),
+   access_time timestamp, updated timestamp with timezone,
+   cases integer DEFAULT NULL, deaths integer DEFAULT NULL,
+   presumptive integer DEFAULT NULL, tested integer DEFAULT NULL,
+   hospitalized integer DEFAULT NULL, negative integer DEFAULT NULL,
+   monitored integer DEFAULT NULL, no_longer_monitored integer DEFAULT NULL,
+   inconclusive integer DEFAULT NULL, pending_tets integer DEFAULT NULL
+   scrape_group REFERENCES scrape_group(id), page_id REFERENCES pages(id))
+ CREATE TABLE IF NOT EXISTS county_data
+  (country_id REFERENCES static.country(id), 
+   state_id REFERENCES static.states(id), 
+   county_id REFERENCES static.county(id),
+   access_time timestamp, updated timestamp with timezone,
+   cases integer DEFAULT NULL, deaths integer DEFAULT NULL,
+   presumptive integer DEFAULT NULL, tested integer DEFAULT NULL,
+   hospitalized integer DEFAULT NULL, negative integer DEFAULT NULL,
+   monitored integer DEFAULT NULL, no_longer_monitored integer DEFAULT NULL,
+   inconclusive integer DEFAULT NULL, pending_tets integer DEFAULT NULL
+   scrape_group REFERENCES scrape_group(id), page_id REFERENCES pages(id))
+
 
 CREATE SCHEMA IF NOT EXISTS static AUTHORIAZATION jesters, reporters
   CREATE TABLE IF NOT EXISTS timezones
@@ -31,8 +66,19 @@ CREATE SCHEMA IF NOT EXISTS static AUTHORIAZATION jesters, reporters
   CREATE TABLE IF NOT EXISTS fips_lut
     (state varchar(2), county_name varchar, fips varchar(5), alt_name varchar)
   CREATE TABLE IF NOT EXISTS urls
-    (state_abb varchar(2), state varchar, url varchar);
+    (state_id REFERENCES state(id), state varchar, url varchar)
+  CREATE TABLE IF NOT EXISTS country
+    (id SERIAL PRIMARY KEY, iso2c varchar(2), iso3c varchar(3),
+     country varchar)
+  CREATE TABLE IF NOT EXISTS states
+    (id SERIAL PRIMARY KEY, abb varchar(2), state varchar)
+  CREATE TABLE IF NOT EXISTS county
+    (id SERIAL PRIMARY KEY, county_name varchar, 
+     state_id integer REFERENCES states(id),
+     fips varchar(5), alt_name varchar DEFAULT NULL, 
+     non_std varchar DEFAULT NULL)
 
 GRANT SELECT ON ALL TABLES IN SCHEMA scraping,static TO reporters;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA scraping TO jesters;
 GRANT SELECT ON ALL TABLES IN SCHEMA static TO jesters;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA static TO ingester;
