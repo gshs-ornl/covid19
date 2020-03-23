ed27ab6ac3c9f72ac7d85266746a0a0665fac70c
1im <mcgahajr@ornl.gov>
2020-03-22 20:33:14 -0400

    Make import from common absolute

--- api/es_app/app.py
+++ api/es_app/app.py
@@ -1,9 +1,8 @@
-from os import environ
 from typing import Any, Dict, List
 
 from flask import Flask, request
 
-from .common import get_var
+from es_app.common import get_var
 
 
 flask_app_name = get_var('FLASK_APP_NAME', 'es_app')
