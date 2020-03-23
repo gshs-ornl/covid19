9a258571939c05a1041bb4fb9aeb24462bc2a7a7
1im <mcgahajr@ornl.gov>
2020-03-23 11:59:55 -0400

    Create eval_list method

--- api/es_app/common.py
+++ api/es_app/common.py
@@ -1,5 +1,6 @@
+from ast import literal_eval
 from os import environ
-from typing import Any
+from typing import Any, Callable, List
 
 
 def get_var(env_var: str, default: Any = None) -> Any:
@@ -20,3 +21,14 @@ def get_var(env_var: str, default: Any = None) -> Any:
     if res := globals().get(env_var):
         return res
     return default
+
+
+def eval_list(list_: str, element_type: Callable = str) -> List[Any]:
+    """Converts list repr to active list using element_type to enforce
+    the type of all elements present in list
+
+    :param list_: list repr string
+    :param element_type: function for asserting type (i.e. str, int, etc.)
+    :return: list object containing elements of desired type
+    """
+    return list(map(element_type, literal_eval(list_)))
