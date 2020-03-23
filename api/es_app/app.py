2b19edfdca4d9a7c9642faa7c34fbbb70485af8e
1im <mcgahajr@ornl.gov>
2020-03-22 20:12:24 -0400

    Move get_var to common for use in parse and app

--- api/es_app/app.py
+++ api/es_app/app.py
@@ -3,25 +3,7 @@ from typing import Any, Dict, List
 
 from flask import Flask, request
 
-
-def get_var(env_var: str, default: Any = None) -> Any:
-    """Scans environment locations for variable
-    Checks in order:
-        os.environ
-        locals()
-        globals()
-
-    :param env_var: Name of variable for grabbing
-    :param default: Value to return if variable not found
-    :return: value of variable or default
-    """
-    if res := environ.get(env_var):
-        return res
-    if res := locals().get(env_var):
-        return res
-    if res := globals().get(env_var):
-        return res
-    return default
+from .common import get_var
 
 
 flask_app_name = get_var('FLASK_APP_NAME', 'es_app')
