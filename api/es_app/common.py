faac9aff04299d22741dd8cfe34fee21eb8a6475
1im <mcgahajr@ornl.gov>
2020-03-22 20:13:13 -0400

    Create basic common module to hold tools for app and parse

---
+++ api/es_app/common.py
@@ -0,0 +1,22 @@
+from os import environ
+from typing import Any
+
+
+def get_var(env_var: str, default: Any = None) -> Any:
+    """Scans environment locations for variable
+    Checks in order:
+        os.environ
+        locals()
+        globals()
+
+    :param env_var: Name of variable for grabbing
+    :param default: Value to return if variable not found
+    :return: value of variable or default
+    """
+    if res := environ.get(env_var):
+        return res
+    if res := locals().get(env_var):
+        return res
+    if res := globals().get(env_var):
+        return res
+    return default
