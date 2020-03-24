d7aff1b4046fdb47b921ce7c3433812e8fa6d294
1im <mcgahajr@ornl.gov>
2020-03-23 20:48:10 -0400

    Correct testing endpoint of Flask app

--- api/es_app/app.py
+++ api/es_app/app.py
@@ -27,7 +27,7 @@ def process_input(uid):
 def check_input(uid):
     start_time = pretty_time()
     content = request.get_json()
-    tmp = ElasticParse(content).entries_to_docs()
+    tmp = ElasticParse(content).gen_actions()
     end_time = pretty_time()
     return {
         'uid': uid,
