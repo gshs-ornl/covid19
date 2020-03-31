e8763824b130c0c16bd3e53d6cffd2e267f0a283
Tansakul <vt1@mac116535.ornl.gov>
2020-03-31 13:02:51 -0400

    Parsers

--- scraper/scripts/iowa.py
+++ scraper/scripts/iowa.py
@@ -4,58 +4,87 @@ import requests
 import datetime
 from numpy import nan
 import pandas as pd
+from cvpy.static import Headers
 
 
 country = 'US'
 url = 'https://services.arcgis.com/vPD5PVLI6sfkZ5E4/ArcGIS/rest/services/IA_COVID19_Cases/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
+url_state = 'https://services.arcgis.com/vPD5PVLI6sfkZ5E4/ArcGIS/rest/services/IACOVID19Cases_Demographics/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=standard&f=pjson&token='
 state = 'Iowa'
 resolution = 'county'
-columns = ['country', 'state', 'url', 'raw_page', 'access_time', 'county',
-           'cases', 'updated', 'deaths', 'presumptive', 'recovered', 'tested',
-           'hospitalized', 'negative', 'counties', 'severe', 'lat', 'lon',
-           'parish', 'monitored', 'private_test', 'state_test',
-           'no_longer_monitored', 'pending_tests', 'active', 'inconclusive',
-           'scrape_group', 'icu',
-           'cases_0_9', 'cases_10_19', 'cases_20_29', 'cases_30_39',
-           'cases_40_49', 'cases_50_59', 'cases_60_69', 'cases_70_79',
-           'cases_80',
-           'hospitalized_0_9', 'hospitalized_10_19', 'hospitalized_20_29',
-           'hospitalized_30_39', 'hospitalized_40_49', 'hospitalized_50_59',
-           'hospitalized_60_69', 'hospitalized_70_79', 'hospitalized_80',
-           'deaths_0_9', 'deaths_10_19', 'deaths_20_29', 'deaths_30_39',
-           'deaths_40_49', 'deaths_50_59', 'deaths_60_69', 'deaths_70_79',
-           'deaths_80']
+columns = Headers.updated_site
+row_csv = []
 
-
+# County level
 raw_data = requests.get(url).json()
 access_time = datetime.datetime.utcnow()
-
-row_csv = []
+resolution = 'county'
 
 for feature in raw_data['features']:
     attribute = feature['attributes']
-
     county = attribute['Name']
-    presumptive_positive = attribute['Confirmed']
+    cases = attribute['Confirmed']
     recovered = attribute['Recovered']
     updated = attribute['last_updated']
     deaths = attribute['Deaths']
 
     row_csv.append([
-        country, state, url, str(raw_data), access_time, county,
-        nan, updated, deaths, presumptive_positive, recovered, nan,
-        nan, nan, nan, nan, nan, nan,
-        nan,  nan, nan, nan,
+        'state', country, state, nan,
+        url, str(raw_data), access_time, county,
+        cases, updated, deaths, nan,
+        recovered, nan, nan, nan,
+        nan, nan, nan, nan, nan,
+        nan,  nan, nan,
+        nan, nan, nan,
+        nan, nan, nan,
+        resolution, nan, nan, nan,
         nan, nan, nan, nan,
+        nan, nan, nan, nan,
+        nan, nan, nan,
         nan, nan,
         nan, nan, nan, nan,
-        nan, nan, nan, nan,
-        nan,
-        nan, nan, nan,
-        nan, nan, nan,
-        nan, nan, nan,
-        nan, nan, nan, nan,
-        nan, nan, nan, nan, nan])
+        nan, nan])
+
+
+# State level - Demographics
+raw_data = requests.get(url_state).json()
+access_time = datetime.datetime.utcnow()
+resolution = 'state'
+age_keys = ['Child_0_17', 'Adult_18_40', 'Middle_Age_41_60',
+            'Older_Adult_61_80', 'Elderly_81']
+other_keys = 'NeverHospitalized'
+
+for feature in raw_data['features']:
+    attribute = feature['attributes']
+    cases = attribute['Total_Cases']
+    cases_male = attribute['Male_Cases']
+    cases_female = attribute['Female_Cases']
+    recovered = attribute['DischRecov']
+    hospitalized = attribute['CurrHospitalized']
+    deaths = attribute['Deceased']
+    other = other_keys
+    other_value = attribute[other_keys]
+
+    for key in age_keys:
+        age_range = key
+        age_cases = attribute[key]
+
+        row_csv.append([
+            'state', country, state, nan,
+            url_state, str(raw_data), access_time, nan,
+            cases, nan, deaths, nan,
+            recovered, nan, hospitalized, nan,
+            nan, nan, nan, nan, nan,
+            nan,  nan, nan,
+            nan, nan, nan,
+            nan, nan, nan,
+            resolution, nan, cases_male, cases_female,
+            nan, nan, nan, nan,
+            age_range, age_cases, nan, nan,
+            nan, nan, nan,
+            nan, nan,
+            nan, nan, nan, nan,
+            other, other_value])
 
 
 df = pd.DataFrame(row_csv, columns=columns)
--- scraper/scripts/maryland.py
+++ scraper/scripts/maryland.py
@@ -4,33 +4,22 @@ import requests
 import datetime
 from numpy import nan
 import pandas as pd
+from cvpy.static import Headers
 
 
 country = 'US'
 url = 'https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/MD_COVID19_Case_Counts_by_County/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=standard&f=pjson&token='
 state = 'Maryland'
 resolution = 'county'
-columns = ['country', 'state', 'url', 'raw_page', 'access_time', 'county',
-           'cases', 'updated', 'deaths', 'presumptive', 'recovered', 'tested',
-           'hospitalized', 'negative', 'counties', 'severe', 'lat', 'lon',
-           'parish', 'monitored', 'private_test', 'state_test',
-           'no_longer_monitored', 'pending_tests', 'active', 'inconclusive',
-           'scrape_group', 'icu',
-           'cases_0_9', 'cases_10_19', 'cases_20_29', 'cases_30_39',
-           'cases_40_49', 'cases_50_59', 'cases_60_69', 'cases_70_79',
-           'cases_80',
-           'hospitalized_0_9', 'hospitalized_10_19', 'hospitalized_20_29',
-           'hospitalized_30_39', 'hospitalized_40_49', 'hospitalized_50_59',
-           'hospitalized_60_69', 'hospitalized_70_79', 'hospitalized_80',
-           'deaths_0_9', 'deaths_10_19', 'deaths_20_29', 'deaths_30_39',
-           'deaths_40_49', 'deaths_50_59', 'deaths_60_69', 'deaths_70_79',
-           'deaths_80']
+columns = Headers.updated_site
 
 
 raw_data = requests.get(url).json()
 access_time = datetime.datetime.utcnow()
 
 row_csv = []
+
+keys_list = ['EOCStatus']
 
 for feature in raw_data['features']:
     attribute = feature['attributes']
@@ -40,21 +29,26 @@ for feature in raw_data['features']:
     recovered = attribute['COVID19Recovered']
     deaths = attribute['COVID19Deaths']
 
-    row_csv.append([
-        country, state, url, str(raw_data), access_time, county,
-        cases, nan, deaths, nan, recovered, nan,
-        nan, nan, nan, nan, nan, nan,
-        nan,  nan, nan, nan,
-        nan, nan, nan, nan,
-        nan, nan,
-        nan, nan, nan, nan,
-        nan, nan, nan, nan,
-        nan,
-        nan, nan, nan,
-        nan, nan, nan,
-        nan, nan, nan,
-        nan, nan, nan, nan,
-        nan, nan, nan, nan, nan])
+    for key in keys_list:
+        other = key
+        other_value = attribute[key]
+
+        row_csv.append([
+            'state', country, state, nan,
+            url, str(raw_data), access_time, county,
+            cases, nan, deaths, nan,
+            recovered, nan, nan, nan,
+            nan, nan, nan, nan, nan,
+            nan, nan, nan,
+            nan, nan, nan,
+            nan, nan, nan,
+            resolution, nan, nan, nan,
+            nan, nan, nan, nan,
+            nan, nan, nan, nan,
+            nan, nan, nan,
+            nan, nan,
+            nan, nan, nan, nan,
+            other, other_value])
 
 
 df = pd.DataFrame(row_csv, columns=columns)
