83a38dae8ab45f4c209cfd31e43c72d35cf69b2c
6nf <grantjn@ornl.gov>
2020-03-30 20:38:23 -0400

    make melt data in postgresql, change run_script; add digester

---
+++ base/cvpy/digester.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Contains the Digest() class which will take the assimilated CSVs."""
+
+
+class Digest():
+    """
+    Clean up output CSV from Ingest and import into the database.
+
+    The Digest class takes the single CSV output from the Ingest() class and
+    """
