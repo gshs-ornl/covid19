c4b2ab39e4290b03b762909adf391f8a0d6ea1a4
6nf <grantjn@ornl.gov>
2020-04-01 19:51:19 -0400

    moved get_all_scripts() and run_script() to common

--- base/cvpy/common.py
+++ base/cvpy/common.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-
+"""Contains common functions used throughout the cvpy package."""
 import os
-""" contains common functions used throughout the cvpy package """
+import logging
+from glob import glob
+from cvpy.error import ScriptError
+from tempfile import TemporaryFile
+from subprocess import check_output, CalledProcessError
 
 
 def check_environment(env_var, default=None):
-    """ check if an environmental variable or variable is set, and if so,
+    """Check for environmental variables in all scopes.
+
+        Check if an environmental variable or variable is set, and if so,
         return that value, else return the default variable
 
         :param env_var the environmental variable to look for
@@ -21,3 +27,31 @@ def check_environment(env_var, default=None):
     if env_var in globals():
         return globals()[env_var]
     return default
+
+
+def get_all_scripts(script_dir,
+                    logger=logging.getLogger(
+                        check_environment('PY_LOGGER', 'main'))):
+    """Retrieve all scripts in the specified directory."""
+    r_scripts = glob(f'{script_dir}/*.R')
+    logger.info(f'Retrieved R scripts {r_scripts}')
+    py_scripts = glob(f'{script_dir}/*.py')
+    logger.info(f'Retrieved Python scripts {py_scripts}')
+    return r_scripts + py_scripts
+
+
+def run_script(script, script_type):
+    """Run a script, capture the output and exit code."""
+    with TemporaryFile() as t:
+        try:
+            if script_type == 'py':
+                out = check_output(['python3', script], stderr=t, stdout=t)
+            if script_type == 'R':
+                out = check_output(['Rscript', script], stderr=t, stout=t)
+            return 0, out
+        except CalledProcessError as e:
+            t.seek(0)
+            msg = f"{script} failed with exit code {e.returncode} " + \
+                f"and message {t.read()}"
+            ScriptError(script, msg).email()
+            return e.returncode, t.read()
