1030cfde48ed81ad70e6db6ee88457687c7eea31
6nf <grantjn@ornl.gov>
2020-04-02 12:33:41 -0400

    explicit logging config, default to local scripts directory as SCRIPT_DIR should be defined within the container env file

--- scraper/py_runner.py
+++ scraper/py_runner.py
@@ -2,20 +2,21 @@
 """Runs all the scripts in the SCRIPT_DIR (/tmp/scripts by default)."""
 import logging
 from datetime import datetime
-from cvpy.common import check_environment as ce
 from cvpy.logging import DictLogger
 from cvpy.common import get_all_scripts
 from cvpy.runners import run_script
+from cvpy.common import check_environment as ce
 # retrieve the logger
 PY_LOGGER = ce('PY_LOGGER', 'main')
-logging.dictConfig(DictLogger.SIMPLE)
+logging.config.dictConfig(DictLogger.SIMPLE)
 logger = logging.getLogger(PY_LOGGER)
 
 
 if __name__ == "__main__":
     run_start = datetime.utcnow()
     logger.info(f'Starting scrape at {run_start}')
-    scripts = get_all_scripts(ce('SCRIPT_DIR', '/tmp/scripts'))
+    script_dir = ce('SCRIPT_DIR', './scripts')
+    scripts = get_all_scripts(script_dir)
     logger.info(f'Found {len(scripts)} to run')
     results = []
     for script in scripts:
