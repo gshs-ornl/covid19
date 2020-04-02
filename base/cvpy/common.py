97cf5589d70a62f5cb3b39204c5122a22628c636
Aaron Myers <myersat@ornl.gov>
2020-04-02 05:53:09 -0400

    function to send emails
    Signed-off-by: Aaron Myers <myersat@ornl.gov>

--- base/cvpy/common.py
+++ base/cvpy/common.py
@@ -59,3 +59,25 @@ def run_script(script, script_type):
                 f"and message {t.read()}"
             ScriptError(script, msg).email()
             return e.returncode, t.read()
+
+def send_email(email_recipients, subject, message_text):
+    """ Send email to the recipients with the given subject and message body
+    text
+
+    :param text:
+    :param text:
+    :param subject:
+    :param args:
+    :return:
+    """
+
+    fromaddr = "covid19scrapers@ornl.gov"
+    toaddr = email_recipients
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message_text, 'plain'))
+
+    server = smtplib.SMTP('smtp.ornl.gov', 25)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
