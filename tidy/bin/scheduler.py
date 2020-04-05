292337c527a2c3984fb55198b8b96f20eece3e07
6nf <grantjn@ornl.gov>
2020-04-04 21:23:38 -0400

    create observers for both Data (when data in input_dir is changed) and Slurp (for when clean data is changed)

--- tidy/bin/scheduler.py
+++ tidy/bin/scheduler.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 """Monitors $INPUT_DIR for any file changes."""
-import sys
 import time
 import logging
+import traceback
 from cvpy.logging import DictLogger
 from cvpy.common import check_environment as ce
-from cvpy.errors import SlurpError
+from cvpy.watch import DataHandler, SlurpHandler
 from watchdog.observers import Observer
-from watchdog,events import LoggingEventHandler
 
 logging.config.dictConfig(DictLogger.SIMPLE)
 logger = logging.getLogger(ce('PY_LOGGER', 'main'))
 
 if __name__ == "__main__":
     input_dir = ce('INPUT_DIR', '/tmp/input')
-    event_handler = LoggingEventHandler()
-    observer = Observer()
+    clean_dir = ce('CLEAN_DIR', '/tmp/clean')
+    clean_observer = Observer()
+    slurp_observer = Observer()
+    clean_observer.schedule(DataHandler, input_dir, recursive=True)
+    slurp_observer.schedule(SlurpHandler, clean_dir, recursive=True)
+    clean_observer.start()
+    slurp_observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        traceback.print_stack()
+        logger.critical('COVID19 Digesters stopped by user kill signal.')
+        clean_observer.stop()
+        slurp_observer.stop()
+    clean_observer.join()
+    slurp_observer.join()
