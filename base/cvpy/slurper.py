8da6dc3409c2dda7c108f083efe1ae34fe646675
6nf <grantjn@ornl.gov>
2020-04-06 23:37:32 -0400

    use common utils: glob_csvs, get_csv, & create_uri; remove passage of csv_file and logic checks; create uri as an attribute, then glob csvs and iterate over them and insert them into the database

--- base/cvpy/slurper.py
+++ base/cvpy/slurper.py
@@ -5,38 +5,42 @@ import logging
 import traceback
 from cvpy.database import Database
 from cvpy.common import check_environment as ce
+from cvpy.common import glob_csvs, get_csv, create_uri
 from cvpy.errors import SlurpError
 
 
 class Slurp():
     """Slurps an $CLEAN_DIR CSV into the database and creates views."""
 
-    def __init__(self, csv_file, clean_dir=ce('CLEAN_DIR', '/tmp/clean'),
+    def __init__(self, clean_dir=ce('CLEAN_DIR', '/tmp/clean'),
                  logger=logging.getLogger(ce('PY_LOGGER', 'main')),
                  view_creator=ce('VIEW_CREATOR', 'create_views.sql')):
         """Initialize the Slurp."""
-        if os.path.exists(self.csv):
-            self.csv = csv_file
-        else:
-            self.csv = os.path.join(clean_dir, csv_file)
-        self.logger = logger
         self.view_creator = view_creator
+        self.logger = logger
+        self.uri = create_uri(self.logger)
+        csvs = glob_csvs(clean_dir, self.logger)
+        for c in csvs:
+            if os.path.exists(c):
+                df = get_csv(c, self.logger)
+                self.process(df, c)
+            else:
+                self.logger.warning(f'File {c} was not found, skipping.')
 
-    def process(self):
+    def process(self, df, c):
         """Process the CSV passed during Slurp initialization."""
         # TODO fill in with better logic
         self.logger.info(f'Proceeding with file {self.csv}')
         try:
             with Database() as db:
-                db.import_raw(self.csv)
-            os.remove(self.csv)
-            self.csv = None
+                db.insert_raw_data(df)
+            os.remove(c)
         # TODO better exception handling
         except OSError as e:
             traceback.print_stack()
-            self.logger.error(f'File removal issue for {self.csv}: {e}')
+            self.logger.error(f'File removal issue for {c}: {e}')
         except Exception as e:
             traceback.print_stack()
-            self.logger.error(f'Problem slurping {self.csv}: {e}')
-            raise SlurpError(f'Slurper: {self.csv}',
+            self.logger.error(f'Problem slurping {c}: {e}')
+            raise SlurpError(f'Slurper: {c}',
                              'Slurping of CSV failed with error: {e}')
