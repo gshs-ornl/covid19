c86094adbb9f3de98322e151549212d4331685ef
6nf <grantjn@ornl.gov>
2020-04-12 17:18:14 -0400

    run slurp on initial files, copy files to a /tmp/output for saving in case things break

--- tidy/bin/scheduler.py
+++ tidy/bin/scheduler.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """Monitors $INPUT_DIR for any file changes."""
+import os
 import time
+import shutil
 import logging
 import traceback
 from cvpy.logging import DictLogger
 from cvpy.common import check_environment as ce
 from cvpy.watch import DataHandler, SlurpHandler, ScrapeHandler
+from cvpy.slurper import Slurp
 from watchdog.observers import Observer
 
 logging.config.dictConfig(DictLogger.SIMPLE)
@@ -13,8 +16,15 @@ logger = logging.getLogger(ce('PY_LOGGER', 'main'))
 
 if __name__ == "__main__":
     input_dir = ce('INPUT_DIR', '/tmp/input')
+    logger.info('Cleaning up from any prior failed runs')
     output_dir = ce('OUTPUT_DIR', '/tmp/output')
     clean_dir = ce('CLEAN_DIR', '/tmp/clean')
+    logger.info(f'Copying existing files from {input_dir} to {output_dir}')
+    for root, dirs, files in os.walk(input_dir):
+        for f in files:
+            if f.endswith('.csv'):
+                shutil.copy(os.path.join(root, f), output_dir)
+    Slurp(path=input_dir)
     logger.info('Creating the input_observer')
     # input_observer = Observer()
     # logger.info('Creating the clean_observer')
