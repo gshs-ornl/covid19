2cf5e7deb4baac1af15c15ba09e98a6cbf758d02
Tansakul <vt1@mac116535.ornl.gov>
2020-04-14 08:36:26 -0400

    added another url

--- scraper/scripts/indianna.py
+++ scraper/scripts/indianna.py
@@ -43,10 +43,60 @@ for feature in raw_data['features']:
         nan, nan, nan, nan,
         nan, nan])
 
+
+def fill_in_df(df_list, dict_info, columns):
+    if isinstance(df_list, list):
+        all_df = []
+        for each_df in df_list:
+            each_df['provider'] = dict_info['provider']
+            each_df['country'] = dict_info['country']
+            each_df['state'] = dict_info['state']
+            each_df['resolution'] = dict_info['resolution']
+            each_df['url'] = dict_info['url']
+            each_df['page'] = str(dict_info['page'])
+            each_df['access_time'] = dict_info['access_time']
+            df_columns = list(each_df.columns)
+            for column in columns:
+                if column not in df_columns:
+                    each_df[column] = nan
+                else:
+                    pass
+            all_df.append(each_df.reindex(columns=columns))
+        final_df = pd.concat(all_df)
+    else:
+        df_list['provider'] = dict_info['provider']
+        df_list['country'] = dict_info['country']
+        df_list['state'] = dict_info['state']
+        df_list['resolution'] = dict_info['resolution']
+        df_list['url'] = dict_info['url']
+        df_list['page'] = str(dict_info['page'])
+        df_list['access_time'] = dict_info['access_time']
+        df_columns = list(df_list.columns)
+        for column in columns:
+            if column not in df_columns:
+                df_list[column] = nan
+            else:
+                pass
+        final_df = df_list.reindex(columns=columns)
+    return final_df
+
+
+url = 'https://coronavirus.in.gov/map-test/covid_report_county.csv'
+raw_data = pd.read_csv(url)
+access_time = datetime.datetime.utcnow()
+raw_data.columns = ['county', 'cases', 'deaths', 'tested']
+
+dict_info_county = {'provider': 'state', 'country': country, "url": url,
+                   "state": state, "resolution": "county",
+                   "page": str(raw_data), "access_time": access_time}
+
+county_df = fill_in_df(raw_data, dict_info_county, columns)
+
+
 now = datetime.datetime.now()
 dt_string = now.strftime("_%Y-%m-%d_%H%M")
 path = os.getenv("OUTPUT_DIR", "")
 file_name = path + state + dt_string + '.csv'
 
-df = pd.DataFrame(row_csv, columns=columns)
+df = pd.concat([pd.DataFrame(row_csv, columns=columns), county_df])
 df.to_csv(file_name, index=False)
