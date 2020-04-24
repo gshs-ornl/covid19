6943ce67b8d0615410212a8e37cf479b40e15b0a
Tansakul <vt1@mac116535.ornl.gov>
2020-04-24 09:54:37 -0400

    changes to florida and vermont parsers

--- scraper/scripts/vermont.py
+++ scraper/scripts/vermont.py
@@ -14,6 +14,8 @@ state_eth_url = """https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/ser
 state_gender_url = """https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/T_VCGI_daily_clean/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=sex&outStatistics=[{"statisticType"%3A"count"%2C"onStatisticField"%3A"OBJECTID"%2C"outStatisticFieldName"%3A"value"}]&resultType=standard&cacheHint=true"""
 state_agegrp_url = """https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/T_VCGI_daily_clean/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=age_group&outStatistics=[{"statisticType"%3A"count"%2C"onStatisticField"%3A"OBJECTID"%2C"outStatisticFieldName"%3A"value"}]&resultType=standard&cacheHint=true"""
 state_info_url = 'https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/county_summary/FeatureServer/0//query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=date+DESC&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=1&sqlFormat=standard&f=pjson&token='
+# county-level data URL
+county_url = 'https://services1.arcgis.com/BkFxaEFNwHqX3tAw/arcgis/rest/services/VT_Counties_Cases/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Cases%20desc&resultOffset=0&resultType=standard&cacheHint=true'
 columns = Headers.updated_site
 row_csv = []
 
@@ -89,16 +91,19 @@ access_time = datetime.datetime.utcnow()
 attribute = raw_data['features'][0]['attributes']
 update_date = float(attribute['date'])
 updated = str(datetime.datetime.fromtimestamp(update_date/1000.0))
-cases = attribute['positive_cases']
+cases = attribute['cumulative_positives']
 tested = attribute['total_tests']
 deaths = attribute['total_deaths']
 hospitalized = attribute['current_hospitalizations']
 monitored = attribute['people_monitored']
 no_longer_monitored = attribute['completed_monitoring']
-other_keys = ['cumulative_positives', 'daily_deaths', 'hosp_pui']
+other_keys = ['daily_deaths', 'hosp_pui', 'positive_cases']
 for other_key in other_keys:
-    other = other_key
     other_value = attribute[other_key]
+    if other_key == 'hosp_pui':
+        other = 'hospitalized under investigation.'
+    else:
+        other = other_key
     row_csv.append([
         'state', country, state, nan,
         url, str(raw_data), access_time, nan,
@@ -115,6 +120,40 @@ for other_key in other_keys:
         nan, nan,
         nan, nan, nan, nan,
         other, other_value])
+
+# County-level data
+resolution = 'county'
+url = county_url
+raw_data = requests.get(url).json()
+access_time = datetime.datetime.utcnow()
+for feature in raw_data['features']:
+    attribute = feature['attributes']
+    county = attribute['CntyLabel']
+    if county != 'Pending Validation':
+        fips = attribute['CNTYGEOID']
+        cases = attribute['Cases']
+        deaths = attribute['Deaths']
+        update_date = float(attribute['DateUpd'])
+        updated = str(datetime.datetime.fromtimestamp(update_date / 1000.0))
+        other = 'new_cases'
+        other_value = attribute[other]
+
+        row_csv.append([
+            'state', country, state, nan,
+            url, str(raw_data), access_time, county,
+            cases, updated, deaths, nan,
+            nan, nan, nan, nan,
+            nan, nan, nan, nan, fips,
+            nan, nan, nan,
+            nan, nan, nan,
+            nan, nan, nan,
+            resolution, nan, nan, nan,
+            nan, nan, nan, nan,
+            nan, nan, nan, nan,
+            nan, nan, nan,
+            nan, nan,
+            nan, nan, nan, nan,
+            other, other_value])
 
 now = datetime.datetime.now()
 dt_string = now.strftime("_%Y-%m-%d_%H%M")
