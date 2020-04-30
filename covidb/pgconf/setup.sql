deda9801fbc35bb3d3894d157b50e1d9d52b8440
Bryan Eaton <eatonbm@ornl.gov>
2020-04-30 16:20:03 -0400

    updated setup.sql with Alex's staging schema

--- covidb/pgconf/setup.sql
+++ covidb/pgconf/setup.sql
@@ -61,6 +61,8 @@ CREATE TABLE IF NOT EXISTS static.county
     alt_name    varchar DEFAULT NULL,
     non_std     varchar DEFAULT NULL
 );
+ALTER TABLE static.county
+    ADD CONSTRAINT const_county_unique UNIQUE (county_name, state_id);
 
 /****FIXME why was this county_code? changed to country_code, why is this not
  mapped to the country or state(contains provinces) table? -- jng ****/
@@ -354,12 +356,164 @@ CREATE TABLE IF NOT EXISTS scraping.melt
   value numeric NOT NULL
 );
 
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE if not exists staging.provider (
+      provider_id TEXT PRIMARY KEY
+    , name TEXT
+    , insert_ts TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
+
+CREATE TABLE if not exists staging.scrapes (
+      scrape_id SERIAL UNIQUE
+    , provider_id TEXT REFERENCES staging.provider(provider_id)
+    , uri TEXT
+    , scraped_ts TIMESTAMP WITH TIME ZONE
+    , doc TEXT
+    , csv_file TEXT -- source CSV file
+    , csv_row INT -- row in the CSV file
+    , PRIMARY KEY (provider_id, uri, scraped_ts)
+);
+
+CREATE TABLE if not exists staging.attr_def (
+      attr TEXT NOT NULL PRIMARY KEY
+    , details TEXT
+);
+
+create table if not exists staging.regions
+(
+      region_id TEXT PRIMARY KEY
+    , resolution TEXT -- country, state, county, etc.
+    , admin_lvl INT GENERATED ALWAYS AS (CHAR_LENGTH(region_id) - CHAR_LENGTH(REPLACE(region_id, '^', ''))) STORED
+    , details JSONB
+);
+
+CREATE TABLE if not exists staging.attr_val (
+      scrape_id INT REFERENCES staging.scrapes(scrape_id) ON DELETE CASCADE
+    , region_id TEXT REFERENCES staging.regions(region_id)
+    , valid_time TEXT -- should be of type 'time with precision'
+    , attr TEXT REFERENCES staging.attr_def(attr)
+    , val TEXT NOT NULL
+    , ext JSONB
+    , parser TEXT
+    , PRIMARY KEY (scrape_id, region_id, valid_time, attr)
+);
+
+create or replace function staging.get_provider(v_provider text) RETURNS int
+    language plpgsql
+as
+$$
+DECLARE _result text := (select provider_id from staging.provider where name = v_provider);
+BEGIN
+    IF _result is null then
+        INSERT INTO staging.provider (name) values (v_provider) RETURNING provider_id into _result;
+    end if;
+    return _result;
+end;
+$$;
+
+create or replace function staging.get_scrape(v_provider text, v_url text, v_access text) RETURNS int
+    language plpgsql
+as
+$$
+DECLARE _result int := (select scrape_id from staging.scrapes where provider_id = v_provider
+                        and scraped_ts = TO_TIMESTAMP(v_access, 'YYYY-MM-DD hh24:MI:SS')
+                        and uri = v_url);
+BEGIN
+    IF _result is null then
+        INSERT INTO staging.scrapes (provider_id, uri, scraped_ts)
+        values (v_provider, v_url, TO_TIMESTAMP(v_access, 'YYYY-MM-DD hh24:MI:SS'))
+        returning scrape_id into _result;
+    end if;
+    return _result;
+end;
+$$;
+
+create or replace function staging.save_region(v_region text, v_region_type text) returns void
+    language plpgsql
+as
+$$
+
+BEGIN
+    INSERT INTO staging.regions (region_id, resolution)
+    values (v_region, v_region_type)
+    ON CONFLICT DO NOTHING;
+end;
+$$;
+
+create or replace procedure staging.save_attribute_data(v_region text, v_region_type text,  v_data jsonb)
+    language plpgsql
+as
+$$
+DECLARE
+    _key      text;
+    _value    text;
+    _provider int;
+    _scrape   int;
+    _access   text := (v_data ->> 'access_time');
+BEGIN
+
+    _provider = staging.get_provider(v_data ->> 'provider');
+    _scrape = staging.get_scrape(_provider, v_data->>'url', _access);
+
+    perform staging.save_region(v_region, v_region_type);
+
+    FOR _key, _value IN
+        SELECT key, value FROM jsonb_each_text(v_data)
+        LOOP
+            --Insert into def table
+            INSERT INTO staging.attr_def (attr, details)
+            values (_key, 'Automated Insert')
+            ON CONFLICT DO NOTHING;
+
+            --Insert into val table
+            INSERT INTO staging.attr_val (scrape_id, region_id, valid_time, attr, val, ext, parser)
+            values (_scrape, v_region, _access, _key, COALESCE(_value, ''), '{}', null)
+            ON CONFLICT DO NOTHING;
+        END LOOP;
+end
+$$;
+
+CREATE OR REPLACE FUNCTION staging.isnumeric(text) RETURNS BOOLEAN AS $$
+DECLARE x NUMERIC;
+BEGIN
+    x = $1::NUMERIC;
+    RETURN TRUE;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE;
+
+/*
+select pid, query, query_start from pg_stat_activity where datname = 'staging' and pid != pg_backend_pid()
+and query != 'SHOW TRANSACTION ISOLATION LEVEL'
+order by query_start desc;
+
+select * from staging.regions where region_id like '%Florida%'
+
+select * from staging.attr_val v
+join staging.regions r ON r.region_id = v.region_id
+where v.val != 'None';
+*/
+
+
+
+-- CALL staging.save_attribute_data ('US^Montana^Beaverhead'::text, 'county'::text, '{"provider": "state", "country": "US", "state": "Montana", "region": null, "url": "https://services.arcgis.com/qnjIrwR8z5Izc0ij/ArcGIS/rest/services/COVID_Cases_Production_View/FeatureServer/0/query?f=json&where=Total%20%3C%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=NewCases%20desc%2CNAMELABEL%20asc&resultOffset=0&resultRecordCount=56&cacheHint=true", "access_time": "2020-04-21 13:24:22.212315", "county": "Beaverhead", "cases": 1.0, "updated": null, "deaths": 0.0, "presumptive": null, "recovered": 0.0, "tested": null, "hospitalized": 0.0, "negative": null, "counties": null, "severe": null, "lat": null, "lon": null, "fips": null, "monitored": null, "no_longer_monitored": null, "pending": null, "active": null, "inconclusive": null, "quarantined": null, "private_tests": null, "state_tests": null, "scrape_group": null, "resolution": "county", "icu": null, "cases_male": null, "cases_female": null, "lab": null, "lab_tests": null, "lab_positive": null, "lab_negative": null, "age_range": "0_9", "age_cases": 0.0, "age_percent": null, "age_deaths": null, "age_hospitalized": null, "age_tested": null, "age_negative": null, "age_hospitalized_percent": null, "age_negative_percent": null, "age_deaths_percent": null, "sex": null, "sex_counts": null, "sex_percent": null, "other": null, "other_value": null}'::jsonb);
+
+
 
 --Functions
 
 GRANT USAGE ON SCHEMA scraping TO reporters, jesters, cvadmin;
 GRANT USAGE ON SCHEMA static TO reporters, jesters, cvadmin;
 GRANT SELECT ON ALL TABLES IN SCHEMA scraping,static TO reporters;
+GRANT USAGE ON SCHEMA staging TO jesters, cvadmin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA staging TO jesters, cvadmin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA staging TO jesters, cvadmin;
+GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA staging TO jesters, cvadmin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA scraping TO jesters, cvadmin;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA scraping, static TO jesters, cvadmin;
 GRANT SELECT ON ALL TABLES IN SCHEMA static TO jesters;
