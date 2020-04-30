09f765a01941c82bc289cb60d3aa1c2d12bec991
Tansakul <vt1@mac116535.ornl.gov>
2020-04-29 22:00:56 -0400

    new parsers

---
+++ scraper/scripts/mississippi.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+from numpy import nan
+import pandas as pd
+from cvpy.static import ColumnHeaders as Headers
+from cvpy.url_helpers import determine_updated_timestep
+
+country = 'US'
+state = 'Mississippi'
+columns = Headers.updated_site
+web_table_url = 'https://msdh.ms.gov/msdhsite/_static/14,0,420.html'
+
+
+def fill_in_df(df_list, dict_info, columns):
+    if isinstance(df_list, list):
+        all_df = []
+        for each_df in df_list:
+            each_df['provider'] = dict_info['provider']
+            each_df['country'] = dict_info['country']
+            each_df['state'] = dict_info['state']
+            each_df['resolution'] = dict_info['resolution']
+            each_df['url'] = dict_info['url']
+            each_df['page'] = str(dict_info['page'])
+            each_df['access_time'] = dict_info['access_time']
+            df_columns = list(each_df.columns)
+            for column in columns:
+                if column not in df_columns:
+                    each_df[column] = nan
+                else:
+                    pass
+            all_df.append(each_df.reindex(columns=columns))
+        final_df = pd.concat(all_df)
+    else:
+        df_list['provider'] = dict_info['provider']
+        df_list['country'] = dict_info['country']
+        df_list['state'] = dict_info['state']
+        df_list['resolution'] = dict_info['resolution']
+        df_list['url'] = dict_info['url']
+        df_list['page'] = str(dict_info['page'])
+        df_list['access_time'] = dict_info['access_time']
+        df_columns = list(df_list.columns)
+        for column in columns:
+            if column not in df_columns:
+                df_list[column] = nan
+            else:
+                pass
+        final_df = df_list.reindex(columns=columns)
+    return final_df
+
+
+df = pd.read_html(web_table_url, match='.*')
+access_time = datetime.datetime.utcnow()
+dict_info_state = {'provider': 'state', 'country': country,
+                   "url": web_table_url,
+                   "state": state, "resolution": "state",
+                   "page": str(df), "access_time": access_time}
+
+dict_info_county = {'provider': 'state', 'country': country,
+                    "url": web_table_url,
+                    "state": state, "resolution": "county",
+                    "page": str(df), "access_time": access_time}
+
+# County-level data
+county_level_df = df[0]
+county_level_df = county_level_df.rename(
+    columns={"County": "county", "Total Cases": 'cases',
+             "Total Deaths": 'deaths',
+             "Total Cases in TLC Facilities": 'other_value'})
+county_level_df['other'] = "Total Cases in TLC Facilities"
+
+cases_deaths_state_level_df = county_level_df[
+    county_level_df['county'] == 'Total'].drop('county', axis=1)
+
+county_level_df = county_level_df[county_level_df['county'] != 'Total']
+
+# Put parsed data in data frame
+county_level_df = fill_in_df(county_level_df, dict_info_county, columns)
+cases_deaths_state_level_df = fill_in_df(cases_deaths_state_level_df,
+                                         dict_info_state, columns)
+
+# State-level test data
+test_df = df[1]
+test_df.columns = ['description', 'tested']
+total_test = test_df[
+    test_df['description'] == "Total individuals tested for COVID-19" \
+                              " statewide"][['tested']]
+other_test = test_df[0:2]
+other_test.columns = ['other', 'other_value']
+
+# Put parsed data in data frame
+state_total_test = fill_in_df(total_test, dict_info_state, columns)
+state_total_lab_test = fill_in_df(other_test, dict_info_state, columns)
+
+now = datetime.datetime.now()
+dt_string = now.strftime("_%Y-%m-%d_%H%M")
+path = os.getenv("OUTPUT_DIR", "")
+file_name = path + state.replace(' ', '_') + dt_string + '.csv'
+
+df = pd.concat([county_level_df, cases_deaths_state_level_df,
+                state_total_test, state_total_lab_test])
+df.to_csv(file_name, index=False)
---
+++ scraper/scripts/northern_marianas_islands.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+from numpy import nan
+import pandas as pd
+from cvpy.static import ColumnHeaders as Headers
+from cvpy.url_helpers import determine_updated_timestep
+
+country = 'US'
+state = 'Northern Marianas Islands'
+columns = Headers.updated_site
+web_table_url = 'http://chcc.gov.mp/coronavirusinformation.php'
+
+
+def fill_in_df(df_list, dict_info, columns):
+    if isinstance(df_list, list):
+        all_df = []
+        for each_df in df_list:
+            each_df['provider'] = dict_info['provider']
+            each_df['country'] = dict_info['country']
+            each_df['state'] = dict_info['state']
+            each_df['resolution'] = dict_info['resolution']
+            each_df['url'] = dict_info['url']
+            each_df['page'] = str(dict_info['page'])
+            each_df['access_time'] = dict_info['access_time']
+            df_columns = list(each_df.columns)
+            for column in columns:
+                if column not in df_columns:
+                    each_df[column] = nan
+                else:
+                    pass
+            all_df.append(each_df.reindex(columns=columns))
+        final_df = pd.concat(all_df)
+    else:
+        df_list['provider'] = dict_info['provider']
+        df_list['country'] = dict_info['country']
+        df_list['state'] = dict_info['state']
+        df_list['resolution'] = dict_info['resolution']
+        df_list['url'] = dict_info['url']
+        df_list['page'] = str(dict_info['page'])
+        df_list['access_time'] = dict_info['access_time']
+        df_columns = list(df_list.columns)
+        for column in columns:
+            if column not in df_columns:
+                df_list[column] = nan
+            else:
+                pass
+        final_df = df_list.reindex(columns=columns)
+    return final_df
+
+
+df = pd.read_html(web_table_url, match='.*')
+access_time = datetime.datetime.utcnow()
+dict_info_state = {'provider': 'state', 'country': country,
+                   "url": web_table_url,
+                   "state": state, "resolution": "state",
+                   "page": str(df), "access_time": access_time}
+
+# State-level test data
+# Remove the first column
+state_df = df[0].drop(0, axis=1)
+state_df.columns = ['data', 'value']
+# Transpose data frame
+state_df = state_df.transpose().reset_index(drop=True)
+# Rename the data frame using the first row
+state_df.columns = state_df.iloc[0]
+# Remove first row as this is duplicated of column names
+state_df = state_df.drop(0, axis=0)
+# Rename columns
+state_df = state_df.rename(
+    columns={'Cumulative Number of Confirmed COVID-19 Cases': "cases",
+             "Cumulative Number of Persons Released from Quarantine": 'quarantined',
+             'Cumulative Number of Deaths': 'deaths',
+             'Cumulative Number of Persons Tested Through Community Testing Initiative': 'other_value'})
+state_df['other'] = 'Cumulative Number of Persons Tested Through Community Testing Initiative'
+
+# Put parsed data in data frame
+state_df = fill_in_df(state_df, dict_info_state, columns)
+
+now = datetime.datetime.now()
+dt_string = now.strftime("_%Y-%m-%d_%H%M")
+path = os.getenv("OUTPUT_DIR", "")
+file_name = path + state.replace(' ', '_') + dt_string + '.csv'
+
+df = state_df
+df.to_csv(file_name, index=False)
