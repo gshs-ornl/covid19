f1b31e07a2d217403036b7e79030cf73848439ef
1im <mcgahajr@ornl.gov>
2020-05-01 15:32:29 -0400

    Add documentation to make_getter

--- api/es_app/common.py
+++ api/es_app/common.py
@@ -25,7 +25,27 @@ def identity(item: Any) -> Any:
 
 
 def make_getter(key: str, *methods: Callable) -> Any:
+    """Creates getter function to operate on gettable object
+
+    The getter function created packages together the key to be
+    used and the functions that should be used to transform the
+    value before returning the value. Functions provided to methods
+    will be conducted in order, so
+    make_getter(key, a, b, c)
+    would make a getter that would act as
+    c(b(a(gettable.get(key))))
+
+    :param key: key to be targeted in gettable
+    :param methods: functions to apply to value before returning
+    :return: getter function
+    """
     def getter(gettable: Any, default: Any = None) -> Any:
+        """Uses get method of gettable and applies methods
+
+        :param gettable: object with get method (default not required)
+        :param default: returned value if gettable.get(key) is None
+        :return: retrieved and operated value or default
+        """
         tmp = gettable.get(key)
         if tmp is None:
             return default
