002e6ed8b3c4f5da6dd077d717c2659ff37e660b
1im <mcgahajr@ornl.gov>
2020-05-05 11:20:51 -0400

    Create test for html checkbox maker

--- api/test/test_interact.py
+++ api/test/test_interact.py
@@ -1,1 +1,23 @@
 from es_app import interact
+
+
+sample_checkbox_checked = '''
+<div>
+    <input type="checkbox" name="sample" value="checked" checked>
+    <label for="sample">sample</label>
+</div>
+'''.strip()
+sample_checkbox_unchecked = '''
+<div>
+    <input type="checkbox" name="sample" value="checked" >
+    <label for="sample">sample</label>
+</div>
+'''.strip()
+
+
+def test_checkbox():
+    assert hasattr(interact, 'checkbox')
+    checkbox = getattr(interact, 'checkbox')
+    assert callable(checkbox)
+    assert checkbox("sample") == sample_checkbox_checked
+    assert checkbox("sample", False) == sample_checkbox_unchecked
