29e337f0f1e5ee928df31cd86eb12f7d3aea5f31
1im <mcgahajr@ornl.gov>
2020-05-05 11:36:45 -0400

    Implement checkbox method

--- api/es_app/interact.py
+++ api/es_app/interact.py
@@ -5,3 +5,16 @@ from time import time
 import zlib
 
 import flask_table
+
+
+def checkbox(name: str, checked: bool = True) -> str:
+    _check = 'checked' if checked else ''
+    _input = f'type="checkbox" name="{name}" value="checked" {_check}'
+    _label = f'<label for="{name}">{name}</label>'
+    body = (
+        '<div>\n'
+        f'    <input {_input}>\n'
+        f'    {_label}\n'
+        '</div>'
+    )
+    return body
