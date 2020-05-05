a7eaddaf85da14b40a1b9a693232f907df031268
Bryan Eaton <eatonbm@ornl.gov>
2020-05-05 13:59:14 -0400

    fixed port

--- covidb/restore_from_production_db.sh
+++ covidb/restore_from_production_db.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 host_name="$1"
-restore_file="$2"
+port="$2"
+restore_file="$3"
 export PGPASSWORD=LovingLungfish
 
 
-echo $1
-echo $2
-psql -h $1 -p 5432 -U cvadmin -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
+psql -h $1 -p $2 -U cvadmin -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
 WHERE pg_stat_activity.datname = 'covidb' -- ← change this to your DB
   AND pid <> pg_backend_pid(); "
 
-psql -h $1 -p 5432 -U cvadmin -d postgres -c "DROP DATABASE IF EXISTS covidb;"
+psql -h $1 -p $2 -U cvadmin -d postgres -c "DROP DATABASE IF EXISTS covidb;"
 
-psql -h $1 -p 5432 -U cvadmin -d postgres -c "CREATE DATABASE covidb with owner 'cvadmin';"
+psql -h $1 -p $2 -U cvadmin -d postgres -c "CREATE DATABASE covidb with owner 'cvadmin';"
 
-pg_restore $2 -h $1 -p 5432 -U cvadmin -d covidb
+pg_restore $3 -h $1 -p $2 -U cvadmin -d covidb
