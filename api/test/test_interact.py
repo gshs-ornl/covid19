a831a0396535d79186f1cb63bc4914297040b771
1im <mcgahajr@ornl.gov>
2020-05-05 12:10:21 -0400

    Create test for html table generation

--- api/test/test_interact.py
+++ api/test/test_interact.py
@@ -1,3 +1,5 @@
+import flask_table
+
 from es_app import interact
 
 
@@ -13,6 +15,10 @@ sample_checkbox_unchecked = '''
     <label for="sample">sample</label>
 </div>
 '''.strip()
+sample_table_data = [
+    {'name': f'name{i}', 'description': f'description{i}'}
+    for i in range(10)
+]
 
 
 def test_checkbox():
@@ -21,3 +27,24 @@ def test_checkbox():
     assert callable(checkbox)
     assert checkbox("sample") == sample_checkbox_checked
     assert checkbox("sample", False) == sample_checkbox_unchecked
+
+
+def test_gen_table():
+    assert hasattr(interact, 'gen_table')
+    gen_table = getattr(interact, 'gen_table')
+    assert callable(gen_table)
+    test_table = flask_table.create_table(
+        'test_table',
+        options={'border': '1px solid black'}
+    )
+    test_table.add_column('name', flask_table.Col('Name'))
+    test_table.add_column('description', flask_table.Col('Description'))
+    test_table_html = test_table(sample_table_data).__html__()
+    gen_test_table = gen_table(
+        'test_table',
+        'Name',
+        'Description',
+        options={'border': '1px solid black'}
+    )
+    gen_test_table_html = gen_test_table(sample_table_data).__html__()
+    assert test_table_html == gen_test_table_html
