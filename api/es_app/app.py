a05db07bc6f71a9b5d0bd849bbb1a289261ad802
1im <mcgahajr@ornl.gov>
2020-05-15 16:07:31 -0400

    Add documentation to app

--- api/es_app/app.py
+++ api/es_app/app.py
@@ -21,6 +21,13 @@ app = Flask(flask_app_name, static_url_path='')
 
 @app.route('/es/put/<uid>', methods=['POST', 'PUT'])
 def process_input(uid):
+    """Accepts posted json for upload to covid19 elasticsearch
+    Endpoint non-operational due to schema changes
+    Use functionality provided by pipe instead for db to db
+
+    :param uid: id value to mark process
+    :return: dictionary with id, start, end time
+    """
     start_time = pretty_time()
     content = request.get_json()
     ElasticParse(content).send_actions()
@@ -34,6 +41,12 @@ def process_input(uid):
 
 @app.route('/es/test-put/<uid>', methods=['POST', 'PUT'])
 def check_input(uid):
+    """Test endpoint meant to demonstrate conversions
+    by process_input
+
+    :param uid: id value to mark the process
+    :return: dictionary with id, start, end, and data converted
+    """
     start_time = pretty_time()
     content = request.get_json()
     tmp = ElasticParse(content).gen_actions()
@@ -48,11 +61,19 @@ def check_input(uid):
 
 @app.route('/es/help')
 def default():
+    """Notes /es/ endpoints
+
+    :return: help text for /es/ endpoints
+    """
     return 'Use /put/<uid> to do things and /test-put/<uid> to test things'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
+    """Loads page for accepting uploaded data for slurping
+
+    :return: completion message
+    """
     if not request.method == 'POST':
         return slurp_html
     file_type = request.form.get('mode')
@@ -72,6 +93,11 @@ def landing():
 
 @app.route('/hack-pipe', methods=['GET'])
 def run_pipe():
+    """Deprecated pipe that used some hacky methods to work
+    for db to db transfer
+
+    :return: progress and task completion text
+    """
     limit = request.args.get('limit', 0, int)
     from_ = request.args.get('from', '')
     to = request.args.get('to', '')
@@ -90,6 +116,10 @@ def run_pipe():
 
 @app.route('/schedule-pipe', methods=['GET'])
 def schedule_background_pipe():
+    """Creates db to db pipe process in a separate thread
+
+    :return: Pipe scheduling success or failure message
+    """
     global current_task
     global pipe_obj
     if current_task is not None and current_task.running():
@@ -107,6 +137,11 @@ def schedule_background_pipe():
 
 @app.route('/check-pipe', methods=['GET'])
 def check_background_pipe():
+    """Checks if there is a pipe task currently running and reports
+    information on its progress and completion state
+
+    :return: progress text or completion information
+    """
     global current_task
     global pipe_obj
     if current_task.running():
